525bd0bc900ebee1f2ffd99617b9c01ac3d5989d
kondurusai <kondurusai863@gmail.com>
2024-06-27 11:27:17 +0530

    initial setup done

---
+++ xspellcheck/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+function App() {
+  const [input, setInput] = useState("");
+  const [val, setVal] = useState("");
+  const customDictionary = {
+    teh: "the",
+
+    wrok: "work",
+
+    fot: "for",
+
+    exampl: "example",
+  };
+  function res(input, customDictionary) {
+    let result = input.split(" ");
+    console.log(result, "this is the splitted value");
+    for (let i = 0; i < result.length; i++) {
+      if (customDictionary[result[i].toLowerCase()]) {
+        return result[i];
+      }
+    }
+    return "";
+  }
+  useEffect(() => {
+    let result = res(input, customDictionary);
+    console.log(result, "this is the returned value");
+    setVal(result);
+  }, [input]);
+
+  console.log(input);
+  console.log(val);
+  return (
+    <div className="App">
+      <h1>Spell Check and Auto-Correction</h1>
+      <div>
+        <textarea
+          placeholder="Enter text..."
+          rows="6"
+          cols="50"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+      </div>
+      {val ? (
+        <p>
+          Did you mean:<bold>{val}</bold>?
+        </p>
+      ) : (
+        <p> </p>
+      )}
+    </div>
+  );
+}
+
+export default App;
